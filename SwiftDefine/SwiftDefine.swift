a281c54e83adee525e3a5d7da8dfc0e51efe9529
jjw <objectclass@163.com>
2016-11-30 19:43:29 +0800

    no message

--- SwiftDefine/SwiftDefine.swift
+++ SwiftDefine/SwiftDefine.swift
@@ -10,31 +10,31 @@ import UIKit
 
 class SwiftDefine: NSObject {
     //MARK: 屏幕高
-    class func screenHeight() ->CGFloat{
+    public class func screenHeight() ->CGFloat{
         return UIScreen.main.bounds.size.height
     }
     //MARK: 屏幕宽
-    class func screenWidth() ->CGFloat{
+    public class func screenWidth() ->CGFloat{
         return UIScreen.main.bounds.size.width
     }
     //MARK: 屏幕尺寸size
-    class func screenBounds() ->CGSize{
+    public class func screenBounds() ->CGSize{
         return UIScreen.main.bounds.size
     }
     //MARK: 是否为3.5英寸
-    class func is_3_5Inch() ->Bool{
+    public class func is_3_5Inch() ->Bool{
         return SwiftDefine.screenHeight() == 480
     }
     //MARK: 是否为4英寸
-    class func is_4Inch() ->Bool{
+    public class func is_4Inch() ->Bool{
         return SwiftDefine.screenHeight() == 568
     }
     //MARK: 是否为4.7英寸
-    class func is_4_7Inch() ->Bool{
+    public class func is_4_7Inch() ->Bool{
         return SwiftDefine.screenHeight() == 667
     }
     //MARK: 是否为5.5英寸
-    class func is_5_5Inch() ->Bool{
+    public class func is_5_5Inch() ->Bool{
         return SwiftDefine.screenHeight() == 736
     }
 
